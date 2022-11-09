05b2b98c287a732bfd104ac995d95e43339b949c
mamun102076 <mamun102076@gmail.com>
2022-11-09 22:09:08 +0600

    loaded all service data from mongodb

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+require('dotenv').config()
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ampetqi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollections = client.db('restaurantGuide').collection('services')
+
+        app.get('/services',async (req,res)=>{
+            const query = {}
+            const cursor = serviceCollections.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err))
 app.get('/',(req,res)=>{
     res.send('restaurant guide server is running')
 })
