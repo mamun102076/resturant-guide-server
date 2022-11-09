a380151a48025afbf1a02ae4e240aaa084b84acd
mamun102076 <mamun102076@gmail.com>
2022-11-10 01:14:19 +0600

    loaded all services and limited the previous sevices

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ async function run(){
         app.get('/services',async (req,res)=>{
             const query = {}
             const cursor = serviceCollections.find(query)
+            const services = await cursor.limit(3).toArray()
+            res.send(services)
+        })
+        app.get('/allservices',async (req,res)=>{
+            const query = {}
+            const cursor = serviceCollections.find(query)
             const services = await cursor.toArray()
             res.send(services)
         })
